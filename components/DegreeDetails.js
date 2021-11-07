3ffd0c72aa530dcc469a273724a24ca28448913b
starpal <paolavee@gmail.com>
2021-11-07 20:55:36 +0100

    put randomSky function in useEffect of 'degreeDetails' to keep it while toggling background

--- components/DegreeDetails.js
+++ components/DegreeDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, ImageBackground, Text, TouchableOpacity } from "react-native";
 import styles from "./styles/styles";
@@ -9,45 +9,49 @@ import { Animated } from "react-native";
 
 export default function DegreeDetails({ toggleSwitch, sign, degree, title, keynote, description }) {
 
-  const navigation = useNavigation();
+	const [randomSky, setRandomSky] = useState();
+	const navigation = useNavigation();
 
-  const {
-	onScroll,
-	containerPaddingTop,
-	scrollIndicatorInsetTop,
-  } = useCollapsibleHeader(OPTIONS);
+	useEffect(() => {
+		setRandomSky(util.getRandomSky())
+	}, [])
+	
+	const {
+		onScroll,
+		containerPaddingTop,
+		scrollIndicatorInsetTop,
+	} = useCollapsibleHeader(OPTIONS);
 
-  return (
-	<ImageBackground source={toggleSwitch ? null : util.getRandomSky()} style={styles.homePageImage}>
-		<Animated.ScrollView
-			onScroll={onScroll}
-			scrollIndicatorInsets={{ top: scrollIndicatorInsetTop, right: 1 }}
-			contentContainerStyle={{
-			paddingVertical: 20,
-			paddingTop: containerPaddingTop
-			}}>
-			<View style={[styles.main, toggleSwitch && { backgroundColor: "#ded9d6" }]}>
-				<Text style={styles.sign}>{sign}</Text>
-				<Text style={[styles.degree, {fontFamily: Platform.OS == "android" && 'Charmonman'}]}>{degree + "°"}</Text>
-				<Text style={styles.title}>{title}</Text>
-				<Text style={styles.keynote }>
-					{keynote}
-					{"\n"}
-				</Text>
-			<View style={[ styles.descriptionContainer, !toggleSwitch && {
-				backgroundColor:
-					Platform.OS == "ios"
-						? "rgba(223, 231, 253, 0.3)"
-						: "rgba(223, 231, 253, 0.4)",
-				}
-				]}>
-					<Text style={styles.description}>{description}</Text>
-			</View>
-			</View>
-			<TouchableOpacity style={styles.goBackButtonContainer} onPress={() => navigation.navigate("Home")}>
-				<Text style={styles.goBackButtonText}>Home</Text>
-			</TouchableOpacity>
-		</Animated.ScrollView>
-	</ImageBackground>
-  );
+  	return (
+		<ImageBackground source={toggleSwitch ? null : randomSky} style={styles.homePageImage}>
+			<Animated.ScrollView
+				onScroll={onScroll}
+				scrollIndicatorInsets={{ top: scrollIndicatorInsetTop, right: 1 }}
+				contentContainerStyle={{
+				paddingVertical: 20,
+				paddingTop: containerPaddingTop
+				}}>
+				<View style={[styles.main, toggleSwitch && { backgroundColor: "#ded9d6" }]}>
+					<Text style={styles.sign}>{sign}</Text>
+					<Text style={[styles.degree, {fontFamily: Platform.OS == "android" && 'Charmonman'}]}>{degree + "°"}</Text>
+					<Text style={styles.title}>{title}</Text>
+					<Text style={styles.keynote }>
+						{keynote}
+						{"\n"}
+					</Text>
+					<View style={[ styles.descriptionContainer, !toggleSwitch && {
+						backgroundColor:
+							Platform.OS == "ios"
+								? "rgba(223, 231, 253, 0.3)"
+								: "rgba(223, 231, 253, 0.4)",
+						}]}>
+						<Text style={styles.description}>{description}</Text>
+					</View>
+				</View>
+				<TouchableOpacity style={styles.goBackButtonContainer} onPress={() => navigation.navigate("Home")}>
+					<Text style={styles.goBackButtonText}>Home</Text>
+				</TouchableOpacity>
+			</Animated.ScrollView>
+		</ImageBackground>
+  	);
 }
