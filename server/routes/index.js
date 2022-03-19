9f909fb709f925c85744bb5ffa3e31b7cd46422e
starpal <paolavee@gmail.com>
2022-03-19 23:55:32 +0100

    update master

---
+++ server/routes/index.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var router = express.Router();
+const ZodiacSymbol = require("../models/zodiacSymbol");
+const uploadBgImage = require("../models/uploadBgImage");
+
+router.get('/', (req, res, next) => {
+	res.status(200).send('zodiac-symbols');
+});
+
+/* GET random background image */
+router.get("/uploads", (req, res, next) => {
+  uploadBgImage.aggregate([{ $sample: { size: 1 } }])
+    .then((response) => {
+      //res.render("main", { images: response[0] });
+      res.json(response[0]);
+    })
+    .catch((error) => {
+      console.log(
+        "Error while getting image from the DB: ",
+        error.message
+      );
+      throw error;
+    });
+});
+
+/* GET random degree. */
+router.get("/degree", (req, res, next) => {
+  ZodiacSymbol.aggregate([{ $sample: { size: 1 } }])
+    .then((response) => {
+      //console.log("dataRandom", response)
+      res.json(response);
+    })
+    .catch((error) => {
+      console.log(
+        "Error while getting the degree from the DB: ",
+        error.message
+      );
+      throw error;
+    });
+});
+
+/* GET degree from DB. */
+router.post("/DBdegree", (req, res, next) => {
+  const { sign, degree } = req.body;
+  ZodiacSymbol.find({ sign, degree })
+    .then((response) => {
+      //console.log("DBdata", response)
+      res.json(response[0]);
+    })
+    .catch((error) => {
+      console.log(
+        "Error while getting the degree from the DB: ",
+        error.message
+      );
+      throw error;
+    });
+});
+
+module.exports = router;
