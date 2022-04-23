dd50acb9a51509b4af5f93d7b300612125490de1
starpal <paolavee@gmail.com>
2022-04-23 21:44:12 +0200

    added fadeIn animation to home component

--- views/Home/index.js
+++ views/Home/index.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import "react-native-gesture-handler";
-import { View, TouchableOpacity, Text, ImageBackground } from "react-native";
+import { View, TouchableOpacity, Text, ImageBackground, Animated } from "react-native";
 import styles from "./styles";
-import homeBg from "../../static/images/MOON3.jpeg"
 
-function HomeScreen({ navigation }) {
+const FadeInView = (props) => {
+	const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+  
+	useEffect(() => {
+	  Animated.timing(
+		fadeAnim,
+		{
+		  toValue: 1,
+		  duration: 1500,
+		  useNativeDriver: true
+		}
+	  ).start();
+	}, [fadeAnim])
+  
+	return (
+	  <Animated.View
+		style={{
+		  ...props.style,
+		  opacity: fadeAnim,
+		}}
+	  >
+		{props.children}
+	  </Animated.View>
+	);
+}
+
+function HomeScreen({ navigation, homeBg }) {
 
   return (
-	<ImageBackground
-	  	source={homeBg}
-		style={styles.homePageImage}>
-		<View style={styles.homeDiv}>
-			<TouchableOpacity style={[styles.button, { width: 220 }]} 
-				onPress={() => navigation.navigate("DBSearch")}>
-				<Text style={styles.buttonText_degrees}>
-					Degrees
-				</Text>
-			</TouchableOpacity>
-			<TouchableOpacity style={[styles.button, { width: 235 }]} 
-				onPress={() => navigation.navigate("Results", {screen: 'Random'})}>
-				<Text style={styles.buttonText_inspired}>
-					..get inspired!
-				</Text>
-			</TouchableOpacity>
-		</View>
-	</ImageBackground>
-	);
+	<FadeInView>
+		<ImageBackground
+			source={homeBg}
+			style={styles.homePageImage}>
+			<View style={styles.homeDiv}>
+				<TouchableOpacity style={[styles.button, { width: 220 }]}
+					onPress={() => navigation.navigate("DBSearch")}>
+					<Text style={styles.buttonText_degrees}>
+						Degrees
+					</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={[styles.button, { width: 235 }]}
+					onPress={() => navigation.navigate("Results", {screen: 'Random'})}>
+					<Text style={styles.buttonText_inspired}>
+						..get inspired!
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</ImageBackground>
+	</FadeInView>
+	)
 }
 
 export default HomeScreen;
